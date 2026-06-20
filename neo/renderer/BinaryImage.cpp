794f1007c13ba206bfef742df11bdb8456fbaa19
nokiavisual-star <nokiavisual@gmail.com>
2026-06-20 18:15:55 +0300

    Update BinaryImage.cpp

--- neo/renderer/BinaryImage.cpp
+++ neo/renderer/BinaryImage.cpp
@@ -37,7 +37,7 @@ If you have questions concerning this license or the applicable additional terms
 */
 
 #include "tr_local.h"
-#include "dxt/DXTCodec.h"
+#include "DXT/DXTCodec.h"
 #include "color/ColorSpace.h"
 
 idCVar image_highQualityCompression( "image_highQualityCompression", "0", CVAR_BOOL, "Use high quality (slow) compression" );
@@ -445,5 +445,3 @@ void idBinaryImage::GetGeneratedFileName( idStr & gfn, const char *name ) {
 	gfn.Replace( ")", "" );
 	gfn.Replace( " ", "" );
 }
-
-
